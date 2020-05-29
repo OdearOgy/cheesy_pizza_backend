4eadbd296f85dc7bb5acbe97818dfeb22b537c9d
OdearOgy <hovhan198@gmail.com>
2020-05-29 05:47:06 +0400

    Fix item model's description field

--- src/item/models.py
+++ src/item/models.py
@@ -10,13 +10,12 @@ from core.utils import save_slug, img_path
 # pizza list
 class Item(models.Model):
     name = models.CharField(max_length=120)
-    description = models.CharField(max_length=500, blank=True)
+    description = models.TextField(max_length=500, blank=True)
     img = models.FileField(upload_to=img_path, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0.0)
     slices = models.IntegerField(default=6)
     slug = models.SlugField(max_length=72, unique=True, blank=True)
-    ingredients = models.ManyToManyField(
-        'Ingredient', blank=True)
+    ingredients = models.ManyToManyField('Ingredient', blank=True)
 
 
     def __str__(self):
